12d2c0917ea3c60ce66c0876d104d374d23770e9
l3creative <nanthony@location3.com>
2013-06-02 22:59:02 -0600

    getting there

--- sites/all/themes/indi/templates/page--front.tpl.php
+++ sites/all/themes/indi/templates/page--front.tpl.php
@@ -15,13 +15,16 @@
 	</div><!-- /#bravo -->
 </div><!-- /.container -->
 
+<div id="breadcrumb-container">
+	<?php print $breadcrumb; ?>
+</div>
+
 <div id="charlie">
 
   <div id="main">
 
     <div id="content" class="column" role="main">
       <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
